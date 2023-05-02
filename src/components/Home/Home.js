4f862754df47e39f62a91e9a1e8c575593233b72
Erica <ericaemorabito@gmail.com>
2023-05-02 15:39:56 -0700

    working on conditional rendering

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,41 +1,80 @@
 import React, { useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import "./home.css";
+import Project from '../Project/Project'
+import Welcome from "../Welcome/Welcome";
 
 const Home = () => {
-  // const [project, setProject ] = useState('');
+  const [project, setProject] = useState("home");
+
+  const handleHome = () => {
+    console.log("home");
+  };
+
+  const handleFirstProject = () => {
+    setProject("first");
+    console.log(`we are on : ${project}`);
+  };
+
+  const handleSecondProject = () => {
+    setProject("second");
+    console.log(`we are on : ${project}`);
+  };
+
+  const handleThirdProject = () => {
+    setProject("third");
+    console.log(`we are on : ${project}`);
+  };
+
+  const handleFourthProject = () => {
+    setProject("fourth");
+    console.log(`we are on : ${project}`);
+  };
+
+  let text;
+  if (project === 'home'){
+    text = <Welcome />
+  } 
+  if (project === 'first'){
+    text = <Project project={project} />
+  } 
+  if (project === 'second'){
+    text = <Project project={project} />
+  } 
+  if (project === 'third'){
+    text = <Project project={project} />
+  } 
 
   return (
     <Row fluid className="screen gx-0">
-      <Col lg={8} md={10} sm={12} xs={12} className="main">
+      <Col lg={12} md={12} sm={12} xs={12} className="main">
         <Row className="body">
 
-          {/* WELCOME AND ABOUT TEXT */}
-          <Col lg={7} md={6} sm={12} xs={12} className="body-col about-col">
-            <h1 id="welcome-text">Welcome</h1>
-            <p id="about-text">
-              I'm a full-stack web developer specialized in the MERN stack based in Austin, Texas.
-              <br></br>I’m passionate about using code to
-              solve problems and improve the lives of others.
-            </p>
+          {/* WELCOME & PROJECT TEXT AREA*/}
+          <Col lg={6} md={12} sm={12} xs={12} className="body-col about-col">
+            {text}
+            
           </Col>
 
           {/* PROJECTS LISTED */}
           <Col lg={5} md={6} sm={12} xs={12} className="body-col projects-col">
             <h2 id="projects-title">Projects</h2>
+            <hr></hr>
             <ul>
               <li>
-              <button>First Project</button>
+                <button onClick={handleHome}>home Project</button>
               </li>
               <li>
-              <button>First Project</button>
+                <button onClick={handleSecondProject}>Second Project</button>
               </li>
               <li>
-              <button>First Project</button>
+                <button onClick={handleThirdProject}>Third Project</button>
+              </li>
+              <li>
+                <button onClick={handleFourthProject}>fourth Project</button>
               </li>
             </ul>
           </Col>
-          
         </Row>
       </Col>
     </Row>
---
+++ src/components/Project/Project.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Project = ({ project }) => {
+
+  return (
+    <>
+      <h2>Project Title</h2>
+      <p> description</p>
+      <ul>
+        <li>tech1</li>
+        <li>tech1</li>
+        <li>tech1</li>
+      </ul>
+      <button>Site</button>
+      <button>Github</button>
+    </>
+  );
+};
+
+export default Project;
